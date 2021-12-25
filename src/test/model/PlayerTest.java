3e9bd09158fa809bdfae41dec3b5314afc52b144
patrickhong21 <95744076+patrickhong21@users.noreply.github.com>
2021-12-24 21:12:44 -0800

    some completed tests for player

--- src/test/model/PlayerTest.java
+++ src/test/model/PlayerTest.java
@@ -1,4 +1,28 @@
 package test.model;
 
+import main.model.Player;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PlayerTest {
+    @BeforeEach
+    public void runBefore() {
+        Player player = new Player("Player");
+    }
+
+    @Test
+    public void testIncreaseScore() {
+        Assertions.assertEquals(1, Player.increaseScore(0));
+    }
+
+    @Test
+    public void testGetName() {
+        Assertions.assertEquals("Player", Player.getName());
+    }
+
+    @Test
+    public void testGetScore() {
+        Assertions.assertEquals(0, Player.getScore());
+    }
 }
