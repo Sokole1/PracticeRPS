6227ac9f42905fe678b2808e9df0cbdb43f4eff5
cadenlu <cadenlu121@gmail.com>
2021-12-24 21:03:40 -0800

    Testing if Caden can upload to github
    TODO:
    finish the ComputerTest
    add imports (i dont know where yet)

--- src/test/model/ComputerTest.java
+++ src/test/model/ComputerTest.java
@@ -1,4 +1,15 @@
 package test.model;
 
 public class ComputerTest {
+
+    @Test
+    public void testIncreaseScore() {
+        /* Assertions.assertEquals(1, Computer.increaseScore(0));
+        TODO:
+        Maybe copy paste player tests into these
+        Add more tests for the computer
+        (for Caden - get the imports for this)
+         */
+
+    }
 }
