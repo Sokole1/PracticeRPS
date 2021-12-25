0d4331dab6f2bbd1e0a9d4c50ce9e2a9947eb378
William <william@gameofapps.org>
2021-12-24 20:31:56 -0800

    Initial Commit

---
+++ src/main/model/Computer.java
@@ -0,0 +1,11 @@
+package main.model;
+
+public class Computer {
+
+    private final String name;
+    private int score = 0;
+
+    public Computer(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/model/Player.java
@@ -0,0 +1,12 @@
+package main.model;
+
+public class Player {
+
+    private final String name;
+    private int score = 0;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = score;
+    }
+}
