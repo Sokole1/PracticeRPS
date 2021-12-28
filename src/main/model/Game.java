483c297926b7ee59e516ba0fc39bad5ab350faa6
cadenlu <cadenlu121@gmail.com>
2021-12-27 20:29:14 -0800

    added the methods + tests for Computer class

--- src/main/model/Game.java
+++ src/main/model/Game.java
@@ -1,4 +1,15 @@
 package main.model;
 
 public class Game {
+    public boolean beatsOther(RPSEnum p, RPSEnum c) {
+        if (p == RPSEnum.ROCK && c == RPSEnum.SCISSORS) {
+            return true;
+        } else if (p == RPSEnum.PAPER && c == RPSEnum.ROCK) {
+            return true;
+        } else if (p == RPSEnum.SCISSORS && c == RPSEnum.PAPER) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/test/model/GameTest.java
+++ src/test/model/GameTest.java
@@ -1,4 +1,31 @@
 package test.model;
 
+
+import main.model.Game;
+import main.model.RPSEnum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GameTest {
+    Game game;
+
+    @BeforeEach
+    public void runBefore() {
+        game = new Game();
+    }
+
+    @Test
+    public void testBeatsOtherWin() {
+        Assertions.assertEquals(true, game.beatsOther(RPSEnum.ROCK, RPSEnum.SCISSORS));
+        Assertions.assertEquals(true, game.beatsOther(RPSEnum.PAPER, RPSEnum.ROCK));
+        Assertions.assertEquals(true, game.beatsOther(RPSEnum.SCISSORS, RPSEnum.PAPER));
+    }
+
+    @Test
+    public void testBeatsOtherLose() {
+        Assertions.assertEquals(false, game.beatsOther(RPSEnum.ROCK, RPSEnum.PAPER));
+        Assertions.assertEquals(false, game.beatsOther(RPSEnum.PAPER, RPSEnum.SCISSORS));
+        Assertions.assertEquals(false, game.beatsOther(RPSEnum.SCISSORS, RPSEnum.ROCK));
+    }
 }
