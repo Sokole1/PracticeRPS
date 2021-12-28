ea3999f2d843ab071db0f07045ff907ea56bcdac
patrickhong21 <95744076+patrickhong21@users.noreply.github.com>
2021-12-27 20:24:44 -0800

    isTie method for game is done

--- src/test/model/GameTest.java
+++ src/test/model/GameTest.java
@@ -2,6 +2,7 @@ package test.model;
 
 import main.model.Game;
 import main.model.Player;
+import main.model.RPSEnum;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ public class GameTest {
     Game game;
     Player player1;
     Player player2;
+
     @BeforeEach
     public void runBefore() {
         game = new Game();
@@ -24,6 +26,30 @@ public class GameTest {
 
         player2 = new Player("player2");
         Assertions.assertFalse(game.hasWon(player2));
+
+    }
+
+    @Test
+    public void testIsTieTrue() {
+        player1 = new Player("player1");
+        player2 = new Player("player2");
+
+        Assertions.assertTrue(game.isTie(RPSEnum.ROCK, RPSEnum.ROCK));
+        Assertions.assertTrue(game.isTie(RPSEnum.SCISSORS, RPSEnum.SCISSORS));
+        Assertions.assertTrue(game.isTie(RPSEnum.PAPER, RPSEnum.PAPER));
+
+
+    }
+
+    @Test
+    public void testIsTieFalse() {
+        player1 = new Player("player1");
+        player2 = new Player("player2");
+
+        Assertions.assertFalse(game.isTie(RPSEnum.SCISSORS, RPSEnum.ROCK));
+        Assertions.assertFalse(game.isTie(RPSEnum.PAPER, RPSEnum.SCISSORS));
+        Assertions.assertFalse(game.isTie(RPSEnum.ROCK, RPSEnum.PAPER));
+
 
     }
 
