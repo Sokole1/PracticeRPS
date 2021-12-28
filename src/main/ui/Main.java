916714142147c68b1e17da08216958ebf0f38fbc
strvight <yifanhao5@gmail.com>
2021-12-27 19:50:55 -0800

    added hasWon() and some random useless UI stuff.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,6 +3,7 @@ package main.ui;
 import javax.swing.*;
 
 public class Main {
+
     public static void main(String[] args) {
         JFrame f=new JFrame("RPS");
 
--- src/test/model/GameTest.java
+++ src/test/model/GameTest.java
@@ -1,4 +1,30 @@
 package test.model;
 
+import main.model.Game;
+import main.model.Player;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class GameTest {
+    Game game;
+    Player player1;
+    Player player2;
+    @BeforeEach
+    public void runBefore() {
+        game = new Game();
+    }
+
+    @Test
+    public void testHasWon() {
+        player1 = new Player("player1");
+        player1.increaseScore();
+        Assertions.assertTrue(game.hasWon(player1));
+
+        player2 = new Player("player2");
+        Assertions.assertFalse(game.hasWon(player2));
+
+    }
+
 }
