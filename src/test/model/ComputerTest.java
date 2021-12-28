94f486df3c99c83df4531547337691a9a23748da
cadenlu <cadenlu121@gmail.com>
2021-12-27 19:57:44 -0800

    added the methods + tests for Computer class

--- src/test/model/ComputerTest.java
+++ src/test/model/ComputerTest.java
@@ -1,15 +1,32 @@
 package test.model;
 
+import main.model.Computer;
+import main.model.Player;
+import main.model.RPSEnum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class ComputerTest {
+    Computer computer;
+    @BeforeEach
+    public void runBefore() {
+        computer = new Computer("Computer");
+    }
+    @Test
+    public void testGetName() {
+        Assertions.assertEquals("Computer", computer.getName());
+    }
 
     @Test
-    public void testIncreaseScore() {
-        /* Assertions.assertEquals(1, Computer.increaseScore(0));
-        TODO:
-        Maybe copy paste player tests into these
-        Add more tests for the computer
-        (for Caden - get the imports for this)
-         */
+    public void testGetScore() {
+        Assertions.assertEquals(0, computer.getScore());
+    }
+    @Test
+    public void testMakeChoice() {
+        computer.makeChoice(new RPSEnum[]{RPSEnum.ROCK});
+        Assertions.assertEquals(RPSEnum.ROCK, computer.makeChoice(new RPSEnum[]{RPSEnum.ROCK}));
 
     }
 }
