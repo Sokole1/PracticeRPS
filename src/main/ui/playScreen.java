71640c17490bd75a11526b3655558e2ebf393b71
William <william@gameofapps.org>
2021-12-29 21:38:50 -0800

    UI almost done connecting, one bug.

--- src/main/ui/playScreen.java
+++ src/main/ui/playScreen.java
@@ -11,20 +11,33 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import main.model.Computer;
+import main.model.Game;
+import main.model.Player;
+import main.model.RPSEnum;
 
-public class
-playScreen extends Application {
+public class playScreen extends Application {
+
+    Stage primaryStage;
+    Label playerScore, computerScore;
+    Game game;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        setUpGame();
 
+        this.primaryStage = primaryStage;
 
         GridPane RPSButtons = new GridPane();
         RPSButtons.setAlignment(Pos.CENTER);
         RPSButtons.setVgap(20);
         RPSButtons.setHgap(25);
 
-        Button rock=new Button ("Rock");
+        Button rock = new Button("Rock");
         Button paper = new Button("Paper");
         Button scissor = new Button("Scissor");
 
@@ -35,6 +48,7 @@ playScreen extends Application {
             public void handle(ActionEvent arg0) {
                 //  Auto-generated method stub
                 System.out.println("rock");
+                handlePlay(RPSEnum.ROCK);
             }
         });
         paper.setOnAction(new EventHandler<ActionEvent>() {
@@ -43,6 +57,7 @@ playScreen extends Application {
             public void handle(ActionEvent arg0) {
                 //  Auto-generated method stub
                 System.out.println("paper");
+                handlePlay(RPSEnum.PAPER);
             }
         });
         scissor.setOnAction(new EventHandler<ActionEvent>() {
@@ -51,6 +66,7 @@ playScreen extends Application {
             public void handle(ActionEvent arg0) {
                 //  Auto-generated method stub
                 System.out.println("scissor");
+                handlePlay(RPSEnum.SCISSORS);
             }
         });
 
@@ -59,10 +75,10 @@ playScreen extends Application {
         HBox scores = new HBox(50);
         scores.setAlignment(Pos.CENTER);
 
-        Label PlayerScore = new Label("Player: ");
-        Label ComputerScore = new Label("Computer: ");
+        playerScore = new Label("Player: " + game.getPlayer().getScore());
+        computerScore = new Label("Computer: " + game.getComputer().getScore());
 
-        scores.getChildren().addAll(PlayerScore, ComputerScore);
+        scores.getChildren().addAll(playerScore, computerScore);
 
         VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
@@ -73,8 +89,31 @@ playScreen extends Application {
         primaryStage.show();
 
     }
-    public static void main(String[] args) {
-        launch(args);
+
+    private void handlePlay(RPSEnum choice) {
+        Player winner = game.play(choice);
+        if (winner != null) {
+            boolean playAgain = GameOverPopUp.display(winner.getName());
+            if (playAgain) {
+                setUpGame();
+                primaryStage.getScene().getWindow().setWidth(primaryStage.getScene().getWidth() + 0.001);
+            } else {
+                primaryStage.close();
+            }
+        } else {
+            playerScore = new Label("Player: " + game.getPlayer().getScore());
+            computerScore = new Label("Computer: " + game.getComputer().getScore());
+            System.out.println(game);
+            primaryStage.getScene().getWindow().setWidth(primaryStage.getScene().getWidth() + 0.001);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Start new game with 2 players with initial score 0
+    private void setUpGame() {
+        Player player1 = new Player("You");
+        Computer computer = new Computer("CPU", new RPSEnum[]{RPSEnum.ROCK, RPSEnum.PAPER, RPSEnum.SCISSORS});
+        game = new Game(player1, computer);
     }
 
 }
